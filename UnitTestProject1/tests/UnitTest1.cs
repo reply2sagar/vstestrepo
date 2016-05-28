0a9c20bfe1d6894be7e83ffbb0b86ba1b4fe14f1
localsagar <sagar@softpost.org>
2016-05-28 17:41:04 +1000

    first push

---
+++ UnitTestProject1/tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("softpost.org", "softpost.org");
+        }
+    }
+}
